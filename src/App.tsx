4a631eee2c7ecd09d8692225dda82817b157e818
benoitlub <benoitlubert@gmail.com>
2026-05-25 23:26:30 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import "./styles/blacklace.css";
+import Index from "./pages/Index.tsx";
+import Map from "./pages/Map.tsx";
+import Aloisia from "./pages/Aloisia.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/aloisia" element={<Aloisia />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
